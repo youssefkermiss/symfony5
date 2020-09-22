b9f03294f55fe9211de98ccdeff46d846a03431d
youssef <osboxes@osboxes.org>
2020-09-22 16:22:35 +0100

    Add twig subscriber

---
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Repository\ConferenceRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Twig\Environment;
+
+class TwigEventSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * @var ConferenceRepository
+     */
+    private $conferenceRepository;
+
+    public function __construct(Environment $twig, ConferenceRepository $conferenceRepository)
+    {
+        $this->twig = $twig;
+        $this->conferenceRepository = $conferenceRepository;
+    }
+
+    public function onControllerEvent($event)
+    {
+        $this->twig->addGlobal('conferences', $this->conferenceRepository->findAll());
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ControllerEvent::class => 'onControllerEvent',
+        ];
+    }
+}
